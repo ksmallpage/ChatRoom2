31a9c79260330dbb34320ec68c7e56576484d1e6
keith smallpage <keith.smallpage@icloud.com>
2018-03-13 10:07:22 -0500

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("192.168.0.112", 9999);
+            client.Send();
+            client.Recieve();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        public static Client client;
+        TcpListener server;
+        public Server()
+        {
+         
+            server = new TcpListener(IPAddress.Parse("192.168.0.112"), 9999);
+            server.Start();
+            Thread t = new Thread(Run);
+            t.Start();
+
+        }
+        public  void Run()
+        {
+            
+
+            while (true)
+            {
+                AcceptClient();
+                string message = client.Recieve();
+                Respond(message);
+            }
+            
+            
+                
+                
+            
+        }
+        private void AcceptClient()
+        {
+                
+            
+                TcpClient clientSocket = default(TcpClient);
+            
+                clientSocket = server.AcceptTcpClient();
+                Console.WriteLine("Connected");
+                NetworkStream stream = clientSocket.GetStream();
+                client = new Client(stream, clientSocket);
+            
+                
+
+               
+            
+            
+        
+        }
+        private void Respond(string body)
+        {
+              
+             client.Send(body);   
+        }
+    }
+}
