024027dc8383565d397e61b500001512f46d486c
MeredithA <meredith.albrecht00@gmail.com>
2018-03-14 13:45:37 -0500

    pushing to pull.

--- Server/Server.cs
+++ Server/Server.cs
@@ -16,25 +16,17 @@ namespace Server
         public static Client client;
         TcpListener server;
         Queue<Message> messages;
-<<<<<<< HEAD
         Dictionary<int, Client> listOfClients;
         int key;
 
         public Server()
         {
             server = new TcpListener(IPAddress.Parse("192.168.0.135"), 9999);
-=======
-        private string clientName;
-
-        public Server()
-        {
-
-            server = new TcpListener(IPAddress.Parse("192.168.0.133"), 9999);
->>>>>>> 0180b6e1862bda97015b3b11d2b08c00d7fb4cfd
             server.Start();
-            listOfClients = new Dictionary<int, Client>();  ///added
+            listOfClients = new Dictionary<int, Client>();
             messages = new Queue<Message>();
         }
+
         public void Run()
         {
             Thread t = new Thread(AcceptClient);
@@ -42,7 +34,7 @@ namespace Server
 
         }
 
-        private void AcceptClient()     ////moved from bottom
+        private void AcceptClient()
         {
             while (true)
             {
@@ -69,53 +61,29 @@ namespace Server
             }
         }
 
-        public void SendMessager()
-        {
-            while (true)
-            {
-                Message sendmeassage = messages.Dequeue();
-                client.Send(sendmeassage);
-            }
-        }
-
         public void MessageSender(Client client)
         {
             while (true)
             {
-<<<<<<< HEAD
                 Message sendmeassage = messages.Dequeue();
                 client.Send(sendmeassage);
                 //Respond(message);
             }
-=======
-                TcpClient clientSocket = default(TcpClient);
-                Console.WriteLine("Awaiting for client Connection");
-                clientSocket = server.AcceptTcpClient();
-                Console.WriteLine("what is your Name?");
-                string clientName = Console.ReadLine();
-                NetworkStream stream = clientSocket.GetStream();
-                client = new Client(stream, clientSocket, clientName);
-                Thread threadMessage = new Thread(new ThreadStart(() => Message(client)));
-                threadMessage.Start();
-            }         
-
->>>>>>> 0180b6e1862bda97015b3b11d2b08c00d7fb4cfd
         }
-
-
-
         private void Respond(Message body)
         {
-             client.Send(body);   
+            client.Send(body);
         }
 
         public void captureClient(int key, Client client)
         {
-           
-
-            listOfClients.Add(key, client);
+        listOfClients.Add(key, client);
         }
-
-
     }
+
 }
+
+
+
+
+
